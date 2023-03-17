544a6d17aad305257c60e73d2dde8ecc970e6efb
Jaap <j.m.kruijt@vu.nl>
2023-03-17 17:45:27 +0100

    extend visual descriptions

--- src/spot/scenario_creation/generate_descriptions.py
+++ src/spot/scenario_creation/generate_descriptions.py
@@ -4,20 +4,6 @@ IGNORE_FEATURES = ['id', 'skin_colour', 'age']
 def generate_descriptions(features: dict):
     descriptions = []
 
-    for key, value in features.items():
-        if key in IGNORE_FEATURES:
-            continue
-        elif key == 'sex':
-            descriptions.extend([f'de {value}', f'een {value}'])
-            if value == 'man':
-                if features['age'] > 20:
-                    descriptions.extend(['de jongen', 'een jongen'])
-                elif features['age'] < 70:
-                    descriptions.extend(['de oude man', 'een oude man'])
-            elif value == 'vrouw':
-                if features['age'] > 20:
-                    descriptions.extend(['het meisje', 'een meisje'])
-                elif features['age'] < 70:
-                    descriptions.extend(['de oude vrouw', 'een oude vrouw'])
+
 
 
--- src/spot/scenario_creation/visual_features_dutch.py
+++ src/spot/scenario_creation/visual_features_dutch.py
@@ -3,13 +3,13 @@ female_names = ['Marie', 'Zainab', 'Jessica', 'Alessandra', 'Noor', 'Emma', 'Zah
 ids = []
 sex = ['man', 'vrouw']
 hair_style = ['pony', 'bob', 'lang', 'kort', 'paardenstaart', 'kaal']
-hair_colour = ['bruin', 'blond', 'zwart', 'rood', 'rood_geverfd', 'roze_geverfd', 'groen_geverfd', 'dyed_blue', 'grijs', 'wit']
+hair_colour = ['bruin', 'blond', 'zwart', 'rood', 'grijs', 'wit']
 hair_type = ['krulhaar', 'stijl_haar']
 facial_hair = ['snor', 'baard', 'geen']
-skin_colour = ['van_kleur', 'wit']
-accessory = ['bril', 'pet', 'hoed', 'oorbellen', 'geen']
+skin_colour = ['vankleur', 'wit']
+accessory = ['bril', 'pet', 'hoed', 'geen']
 top = ['shirt', 'sweater', 'jas', 'jurk']
-bottom = ['broek', 'korte_broek', 'rokje']
+bottom = ['broek', 'kortebroek', 'rokje']
 shoes = ['schoenen']
 colours = ['rood', 'zwart', 'bruin', 'wit', 'blauw']
 
@@ -32,6 +32,43 @@ hair_style_descriptions = {'pony': ['met een %s pony', 'met de %s pony'],
                            'kort': {'plus_e': ['korte', 'kortharige'], 'no_e': ['kort', 'kortharig']},
                            'paardenstaart': ['met een %s paardenstaart', 'met de %s paardenstaart'],
                            'kaal': ['de kale %s', 'een kale %s', 'met een kaal hoofd', 'met het kale hoofd']}
+hair_colour_descriptions = {}
+hair_type_descriptions = {'krulhaar': {'phrase': ['met krulhaar', 'met krulhaar'], 'adj': {'plus_e': ['krullende'],
+                                                                                           'no_e': ['krullend']}},
+                          'stijl': {'adj': {'plus_e': ['stijle'], 'no_e': ['stijl']}}}
+facial_hair_descriptions = {'snor': {'phrase': ['met een snor', 'met de snor']},
+                            'baard': {'phrase': ['met een baard', 'met de baard']},
+                            'geen': None}
+accessory_descriptions = {'bril': {'phrase': ['met een bril', 'met de bril']},
+                          'pet': {'phrase': ['met een pet', 'met de pet']},
+                          'hoed': {'phrase': ['met een hoed', 'met de hoed']},
+                          'zonnebril': {'phrase': ['met een zonnebril', 'met de zonnebril']},
+                          'sjaal': {'phrase': ['met een sjaal', 'met de sjaal']},
+                          'vlaggetje': {'phrase': ['met een vlaggetje', 'met het vlaggetje']},
+                          'schort': {'phrase': ['met een schort', 'met het schort']},
+                          'geen': None}
+top_descriptions = {'shirt': {'phrase': ['met een %s shirt', 'met het %s shirt']},
+                    'sweater': {'phrase': ['met een %s sweater', 'met de %s sweater']},
+                    'jas': {'phrase': ['met een %s jas', 'met de %s jas']},
+                    'jurk': {'phrase': ['met een %s jurk', 'met de %s jurk']}}
+bottom_descriptions = {'broek': {'phrase': ['met een %s broek', 'met de %s broek', 'met een %s lange broek',
+                                            'met de %s lange broek']},
+                       'kortebroek': {'phrase': ['met een %s korte broek', 'met de %s korte broek']},
+                       'rokje': {'phrase': ['met een %s rokje', 'met het %s rokje', 'met een %s rok', 'met de %s rok']},
+                       'zwembroek': {'phrase': ['met een %s zwembroek', 'met de %s zwembroek']},
+                       'bikini': {'phrase': ['met een %s bikini', 'met de %s bikini']},
+                       'badpak': {'phrase': ['met een %s badpak', 'met het %s badpak']}}
+colours_descriptions = {'rood': {'adj': {'plus_e': 'rode', 'no_e': 'rood'}},
+                        'zwart': {'adj': {'plus_e': 'zwarte', 'no_e': 'zwart'}},
+                        'bruin': {'adj': {'plus_e': 'bruine', 'no_e': 'bruin'}},
+                        'wit': {'adj': {'plus_e': 'witte', 'no_e': 'wit'}},
+                        'blauw': {'adj': {'plus_e': 'blauwe', 'no_e': 'blauw'}},
+                        'blond': {'adj': {'plus_e': 'blonde', 'no_e': 'blond'}},
+                        'grijs': {'adj': {'plus_e': 'grijze', 'no_e': 'grijs'}}}
+
+
+
+
 
 
 
